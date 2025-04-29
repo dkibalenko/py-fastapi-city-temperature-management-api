0984e65fd86f908eec83971ac86ece3d6211973f
Denys Kibalenko <dennieking87@gmail.com>
2025-04-29 18:52:12 +0200

    Feat: add `get_all_cities` async function for fetching all cities form db & validate them over pydantic `City` model

--- app/crud/city.py
+++ app/crud/city.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 import models, schemas
 
 
-async def create_city(db: AsyncSession, city: schemas.CityCreate):
+async def create_city(db: AsyncSession, city: schemas.CityCreate) -> dict:
     query = insert(models.City).values(
         name=city.name,
         additional_info=city.additional_info
@@ -14,3 +14,11 @@ async def create_city(db: AsyncSession, city: schemas.CityCreate):
     resp = {**city.model_dump(), "id": result.lastrowid}
 
     return resp
+
+
+async def get_all_cities(db: AsyncSession) -> list[schemas.City]:
+    query = select(models.City)
+    city_list = await db.execute(query)
+    return [
+        schemas.City.model_validate(city[0]) for city in city_list.fetchall()
+    ]
