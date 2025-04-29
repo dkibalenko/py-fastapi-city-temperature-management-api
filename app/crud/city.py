0d8f4062aa72218036009ee1d8571b444c21ceff
Denys Kibalenko <dennieking87@gmail.com>
2025-04-29 12:37:12 +0200

    Feat: add app/crud/city.py with `create_city` async function that constructs query, awaits for execution, commitment & generates response

---
+++ app/crud/city.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from app import models, schemas
+
+
+async def create_city(db: AsyncSession, city: schemas.CityCreate):
+    query = insert(models.City).values(
+        name=city.name,
+        additional_info=city.additional_info
+    )
+    result = await db.execute(query)
+    await db.commit()
+    resp = {**city.model_dump(), "id": result.lastrowid}
+
+    return resp
