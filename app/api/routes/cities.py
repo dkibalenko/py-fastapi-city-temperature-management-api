aa4060a62f9f4a6759375d2f2ee30f73133eae00
Denys Kibalenko <dennieking87@gmail.com>
2025-04-29 18:53:14 +0200

    Feat: add `cities` get operation enpoint to fetch all cities from db

--- app/api/routes/cities.py
+++ app/api/routes/cities.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from dependecies import get_db
-from crud.city import create_city
+from crud.city import create_city, get_all_cities
 import schemas
 
 
@@ -12,3 +12,8 @@ router = APIRouter()
 @router.post("/cities/", response_model=schemas.City)
 async def cities(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
     return await create_city(db=db, city=city)
+
+
+@router.get("/cities/", response_model=list[schemas.City])
+async def cities(db: AsyncSession = Depends(get_db)):
+    return await get_all_cities(db=db)
