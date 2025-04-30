6a8c78633aaa866c435507f9be87b8cfa2832168
Denys Kibalenko <dennieking87@gmail.com>
2025-04-30 12:40:17 +0200

    Feat: add `cities` endpoint for `get` single object operation

--- app/api/routes/cities.py
+++ app/api/routes/cities.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from dependecies import get_db
-from crud.city import create_city, get_all_cities
+from crud.city import create_city, get_all_cities, get_single_city
 import schemas
 
 
@@ -17,3 +17,8 @@ async def cities(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
 @router.get("/cities/", response_model=list[schemas.City])
 async def cities(db: AsyncSession = Depends(get_db)):
     return await get_all_cities(db=db)
+
+
+@router.get("/cities/{city_id}", response_model=schemas.City)
+async def cities(city_id: int, db: AsyncSession = Depends(get_db)):
+    return await get_single_city(db=db, city_id=city_id)
