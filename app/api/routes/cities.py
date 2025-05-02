33c32d4229f4c94be923f03428cbd0ba2394dd4f
Denys Kibalenko <dennieking87@gmail.com>
2025-05-02 12:46:35 +0200

    Refactor: rename enpoints `create_city`, `read_cities` & `read_single_city` in app/api/routes/cities.py

--- app/api/routes/cities.py
+++ app/api/routes/cities.py
@@ -10,17 +10,20 @@ router = APIRouter()
 
 
 @router.post("/cities/", response_model=schemas.City)
-async def cities(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
+async def create_city(
+    city: schemas.CityCreate,
+    db: AsyncSession = Depends(get_db)
+):
     return await crud_city.create_city(db=db, city=city)
 
 
 @router.get("/cities/", response_model=list[schemas.City])
-async def cities(db: AsyncSession = Depends(get_db)):
+async def read_cities(db: AsyncSession = Depends(get_db)):
     return await crud_city.get_all_cities(db=db)
 
 
 @router.get("/cities/{city_id}", response_model=schemas.City)
-async def cities(city_id: int, db: AsyncSession = Depends(get_db)):
+async def read_single_city(city_id: int, db: AsyncSession = Depends(get_db)):
     return await crud_city.get_single_city(db=db, city_id=city_id)
 
 
