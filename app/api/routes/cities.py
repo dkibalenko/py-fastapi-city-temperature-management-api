3e358f2a833de5534b924c130a2882e7335ad759
Denys Kibalenko <dennieking87@gmail.com>
2025-05-02 12:44:05 +0200

    Feat: add `city_update` endpoint for `put` operation on city object

--- app/api/routes/cities.py
+++ app/api/routes/cities.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from dependecies import get_db
-from crud.city import create_city, get_all_cities, get_single_city
+import crud.city as crud_city
 import schemas
 
 
@@ -11,14 +11,23 @@ router = APIRouter()
 
 @router.post("/cities/", response_model=schemas.City)
 async def cities(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
-    return await create_city(db=db, city=city)
+    return await crud_city.create_city(db=db, city=city)
 
 
 @router.get("/cities/", response_model=list[schemas.City])
 async def cities(db: AsyncSession = Depends(get_db)):
-    return await get_all_cities(db=db)
+    return await crud_city.get_all_cities(db=db)
 
 
 @router.get("/cities/{city_id}", response_model=schemas.City)
 async def cities(city_id: int, db: AsyncSession = Depends(get_db)):
-    return await get_single_city(db=db, city_id=city_id)
+    return await crud_city.get_single_city(db=db, city_id=city_id)
+
+
+@router.put("/cities/{city_id}", response_model=schemas.City)
+async def city_update(
+    city_id: int,
+    city: schemas.CityUpdate,
+    db: AsyncSession = Depends(get_db)
+):
+    return await crud_city.update_city(db=db, city_id=city_id, city=city)
