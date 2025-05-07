9e912f27f3b4b49c908449d2f84bf5d5b03b5c2d
Denys Kibalenko <dennieking87@gmail.com>
2025-05-07 08:56:38 +0200

    Feat: include api.rotues.temperature.router as `temperature_router` into `app` in app/main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from api.routes.cities import router as city_router
+from api.routes.temperatures import router as temperature_router
 	
 app = FastAPI()
 
 app.include_router(city_router)
+app.include_router(temperature_router)
 	
 	
 @app.get("/")
