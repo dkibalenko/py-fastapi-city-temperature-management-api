37effde3a49c8322966622f69420cba613e94d96
Denys Kibalenko <dennieking87@gmail.com>
2025-05-07 15:29:47 +0200

    Refactor: add `get_logger` for setting up a logger object and return it & add docstings to utils functions to app/utils.py

--- app/utils.py
+++ app/utils.py
@@ -7,18 +7,28 @@ from sqlalchemy import insert, update
 import models
 
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    format="{asctime} - {levelname} - {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M",
-    level=logging.INFO
-)
+def get_logger(name: str) -> logging.Logger:
+    """Return a logger object with custom settings."""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M"
+    )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+logger = get_logger(__name__)
 
 
 def create_city_temperature_records(
         city_temperature_data: List[dict]
 ) -> List[dict]:
+    """Creates a list of temperature records for cities."""
+
     return [
         {
             "city_id": city_id,
@@ -33,6 +43,14 @@ async def insert_new_temperature_records(
         db: AsyncSession,
         new_records: List[dict]
 ) -> str:
+    """
+    Inserts new temperature records for cities into the database.
+
+    :param db: an active database session
+    :param new_records: a list of dicts with keys "city_id" and "temperature"
+    :return: a success message if the new records were inserted,
+             otherwise an error message
+    """
     insert_query = insert(models.Temperature).returning(models.Temperature)
     try:
         await db.execute(insert_query, new_records)
@@ -49,6 +67,14 @@ async def update_existing_temperature_records(
         db: AsyncSession,
         existing_records: List[dict]
 ) -> str:
+    """
+    Updates existing temperature records for cities in the database.
+
+    :param db: an active database session
+    :param existing_records: a list of dicts with keys "city_id", "temperature"
+    :return: a success message if the existing records were updated,
+             otherwise an error message
+    """
     try:
         await db.execute(update(models.Temperature), existing_records)
         await db.commit()
