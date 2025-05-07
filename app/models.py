ee418f9baa09a9e492e89a5b2b23e01738b50470
Denys Kibalenko <dennieking87@gmail.com>
2025-05-07 08:24:16 +0200

    Feat: add SQLAlchemy model `Temperature` with foreign key relation to  `City` model to app/models.py & create migrations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -9,3 +10,22 @@ class City(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     additional_info = Column(String, index=True)
+    temperatures = relationship(
+        "Temperature",
+        back_populates="city",
+        cascade="all, delete-orphan"
+    )
+
+
+class Temperature(Base):
+    __tablename__ = "temperatures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date_time = Column(DateTime, nullable=False)
+    temperature = Column(Float, nullable=False)
+    city_id = Column(
+        Integer,
+        ForeignKey("cities.id"),
+        nullable=False
+    )
+    city = relationship("City", back_populates="temperatures")
