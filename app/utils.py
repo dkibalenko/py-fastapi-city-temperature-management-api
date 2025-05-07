32b46781ea2e5b7300ca7944c90d01e353e4020f
Denys Kibalenko <dennieking87@gmail.com>
2025-05-07 15:11:48 +0200

    Feat: add `logger`, `insert_new_temperature_records` & `update_existing_temperature_records` async functions to app/utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,20 @@
 from typing import List
+import logging
+
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy import insert, update
+
+import models
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.INFO
+)
+
 
 def create_city_temperature_records(
         city_temperature_data: List[dict]
@@ -8,5 +24,37 @@ def create_city_temperature_records(
             "city_id": city_id,
             "temperature": temperature,
         }
-        for city_id, temperature in city_temperature_data.items()
+        for city_id, temperature
+        in city_temperature_data.items()
     ]
+
+
+async def insert_new_temperature_records(
+        db: AsyncSession,
+        new_records: List[dict]
+) -> str:
+    insert_query = insert(models.Temperature).returning(models.Temperature)
+    try:
+        await db.execute(insert_query, new_records)
+        await db.commit()
+    except Exception as e:
+        await db.rollback
+        logger.error(f"Error inserting temperatures: {e}")
+        return
+
+    return f"New temperatures records for {new_records} created."
+
+
+async def update_existing_temperature_records(
+        db: AsyncSession,
+        existing_records: List[dict]
+) -> str:
+    try:
+        await db.execute(update(models.Temperature), existing_records)
+        await db.commit()
+    except Exception as e:
+        await db.rollback()
+        logger.error(f"Error updating temperatures: {e}")
+        return
+
+    return f"Updated existing temperature records for {existing_records}."
