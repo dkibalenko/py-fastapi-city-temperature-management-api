9480e2dda32ed017e5f3b4db5ad3a348e6aa5df7
Denys Kibalenko <dennieking87@gmail.com>
2025-05-09 10:11:50 +0200

    Refactor: remove `read_single_temperature` endpoint from app/api/routes/temperatures.py

--- app/api/routes/temperatures.py
+++ app/api/routes/temperatures.py
@@ -28,26 +28,7 @@ async def read_temperatures(
     if not temperatures:
         raise HTTPException(
             status_code=404,
-            detail="No temperatures found."
+            detail=f"No temperatures found for city with ID: {city_id}."
         )
 
     return temperatures
-
-
-@router.get("/temperture/{temp_id}/", response_model=schemas.Temperature)
-async def read_single_temperature(
-    temp_id: int,
-    db: AsyncSession = Depends(get_db)
-):
-    temperature = await crud_temperature.read_single_temperature_record(
-        db=db,
-        temp_id=temp_id
-    )
-
-    if not temperature:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Temperature with id {temp_id} not found."
-        )
-
-    return temperature
