734e07b5738ce9fd022f812d1bc8c66a834b024b
Denys Kibalenko <dennieking87@gmail.com>
2025-05-12 08:33:44 +0200

    Refactor: change imports to `from app.models` in app/alembic/env.py

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models import *
-from database import Base
+from app.models import *
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
