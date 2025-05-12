fec9809a942857317fa38ffb17b6b724b5cd9bbe
Denys Kibalenko <dennieking87@gmail.com>
2025-05-12 19:57:19 +0200

    Refactor: remove `connect_args` option from `create_async_engine` in app/database.py

--- app/database.py
+++ app/database.py
@@ -10,7 +10,6 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(
